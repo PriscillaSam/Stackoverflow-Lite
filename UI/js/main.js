e53b4c058d178a43a7a2503cb746328a08114f82
PriscillaSam <samiduh@live.com>
2018-08-07 18:54:55 +0100

    add login form to register page

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,5 +1,9 @@
 const inputs = document.getElementsByClassName('form-input');
-
+const signupSection = document.getElementById('signup');
+const loginSection = document.getElementById('login');
+const signupToggle = document.getElementById('signup-toggle');
+const loginToggle = document.getElementById('login-toggle');
+const container = document.getElementsByClassName('center')[0];
 
 [...inputs].forEach(i  => {
 
@@ -11,3 +15,28 @@ const enlarge = (input) => {
 
     input.style.padding = '50px';
 };
+
+signupToggle.addEventListener('click', (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+        loginSection.classList.add('hidden','fadeOut');
+        signupSection.classList.remove('hidden','fadeOut');
+        container.style.margin = '80px auto';
+        signupSection.classList.add('fadeIn');
+
+    });
+  
+});
+
+loginToggle.addEventListener('click', (event) => {
+    event.preventDefault();
+    signupSection.classList.add('fadeOut');
+    setTimeout(() => {
+        signupSection.classList.add('hidden');
+        container.style.margin = '150px auto';
+        loginSection.classList.remove('hidden');
+        loginSection.classList.add('fadeIn');
+
+    });
+
+});
