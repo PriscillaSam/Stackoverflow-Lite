450181bff5b819048c43ec55d813e93335b60c04
PriscillaSam <samiduh@live.com>
2018-08-09 11:32:40 +0100

    create user profile

--- UI/js/app.js
+++ UI/js/app.js
@@ -3,13 +3,26 @@ const nav = document.getElementById('nav-sm');
 
 [...toggle].forEach(t => {
     
-    t.addEventListener('click', () => {
+    t.addEventListener('click', (event) => {
+        event.preventDefault();
+
         if(nav.classList.contains('hidden')){
+            if(nav.classList.contains('slideOutLeft')){
+                nav.classList.remove('slideOutLeft');
+            }
+           
+            nav.classList.add('slideInLeft');            
             nav.classList.remove('hidden');
 
         }
         else{
-            nav.classList.add('hidden');
+            nav.classList.remove('slideInLeft');
+
+            setTimeout(() => {
+                nav.classList.add('hidden');
+            },1000);
+            nav.classList.add('slideOutLeft');
+
         }
     })
 });
