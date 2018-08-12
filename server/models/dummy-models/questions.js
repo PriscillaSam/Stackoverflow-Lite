60a3ce19b310a900c7203993849fdca3b3407caa
PriscillaSam <samiduh@live.com>
2018-08-12 23:57:13 +0100

    add dummy models

---
+++ server/models/dummy-models/questions.js
@@ -0,0 +1,25 @@
+const questions = [
+  {
+    id: 1,
+    userId: 1,
+    question: 'Why is programming hard?',
+  },
+  {
+    id: 2,
+    userId: 7,
+    question: 'Cooking is a skill or inborn?',
+  },
+  {
+    id: 3,
+    userId: 7,
+    question: 'Why do we have seasons and weather?',
+  },
+  {
+    id: 4,
+    userId: 8,
+    question: 'What is the maximum height a snake can climb?',
+  },
+
+];
+
+export default questions;
