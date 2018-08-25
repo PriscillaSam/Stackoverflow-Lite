fe0e3e16b1fbc92bd90987f218deaebb61340771
PriscillaSam <samiduh@live.com>
2018-08-25 19:32:03 +0100

    :sparkles:modify api endpoint to get a question to use a database
    - write sql queries to get question, answers and votes from database
    
    - refactor tests to use database
    - [Finishes #160038467]

--- server/controllers/question.js
+++ server/controllers/question.js
@@ -4,7 +4,7 @@ import errors from '../helpers/errorMessages';
 import pool from '../config/db.config';
 import queries from '../helpers/queries';
 
-const { questionQueries } = queries;
+const { answerQueries, questionQueries } = queries;
 
 /**
  * Question controller
@@ -40,16 +40,27 @@ class Question {
   static getQuestion(req, res) {
     const questionId = parseInt(req.params.id, 10);
 
-    const question = repo.getQuestion(questionId);
-    if (question === null) {
-      return errors.notFound(res, 'question');
-    }
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'question has been successfully gotten',
-      question,
-    });
+    pool.connect()
+      .then((client) => {
+        client.query(questionQueries.getQuestion(questionId))
+          .then((response) => {
+            client.release();
+            const [questionObj] = response.rows;
+            if (!questionObj) {
+              return errors.notFound(res, 'question');
+            }
+            client.query(answerQueries.getAnswersByQId(questionId))
+              .then((answerResponse) => {
+                const answers = answerResponse.rows;
+                questionObj.answers = answers;
+                res.status(200).json({
+                  status: 'success',
+                  message: 'question has been successfully gotten',
+                  questionObj,
+                });
+              });
+          });
+      });
   }
 
   /**
